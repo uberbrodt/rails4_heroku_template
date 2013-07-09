ef7df292927fb0c70c2193db17e21422f3037b7b
Chris Brodt <chris@path.to>
2013-07-09 14:37:23 -0400

    Customize the spec_helper.rb

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
 Use it like this:
 
-rails new [APP NAME] -T -m https://raw.github.com/uberbrodt/rails4_heroku_template/master/rails4_heroku.rb
+rails new [APP NAME] -T -B -m https://raw.github.com/uberbrodt/rails4_heroku_template/master/rails4_heroku.rb
+
+
+What this does is prevent TestUnit stuff from being generated (because we use RSpec), and does not run Bundle Install
+(the template runs it already so it gets access to some generators in the gems it includes)
--- rails4_heroku.rb
+++ rails4_heroku.rb
@@ -60,3 +60,72 @@ end
 gem_group :production, :staging do
   gem 'rails_12factor'
 end
+
+run "bundle install"
+
+generate("rspec:install")
+
+file "spec/spec_helper.rb", <<-CODE
+require 'simplecov'
+SimpleCov.start 'rails'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'rspec/given'
+require 'capybara/rspec'
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include FactoryGirl::Syntax::Methods
+  #config.fixture_path = "\#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+CODE
